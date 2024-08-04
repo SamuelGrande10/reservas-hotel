286fa412d67e0d13d2fe422bf7fcc6c268272d9b
Estefany-s <70726379+Estefany-s@users.noreply.github.com>
2024-08-04 16:14:13 -0600

    Agregando la ruta para ver las habitaciones por hotel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./components/Login.css";
-import InicioSesion from "./pages/InicioSesion";
-import Register from "./components/Register";
-import Reservar from "./pages/Reservar";
-import RecoverPassword from "./components/RecoverPassword";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Bedrooms from "./components/Bedrooms";
+import InicioSesion from "./pages/InicioSesion";
+import RecoverPassword from "./components/RecoverPassword";
+import Register from "./components/Register";
+import Reservar from "./pages/Reservar";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/reservar" element={<Reservar />} />
         <Route path="/recuperarContrasena" element={<RecoverPassword />} />
+        <Route path="/hotel/:id" element={<Bedrooms />} />
       </Routes>
     </Router>
   );
