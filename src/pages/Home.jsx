65471615d33d753b4fe6ea0728fd1070675e2f37
Estefany-s <70726379+Estefany-s@users.noreply.github.com>
2024-08-21 23:06:34 -0600

    Primer avance del home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,44 @@
-import Navbar from "../components/Navbar";
+import Navbar from '../components/Navbar';
+import '../components/Home.css';
+import { hotels } from "../data/hotels.json";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <h1>Este es el Home</h1>
-      <p>Aqui podria ir como hoteles y alguna imagen creo haha</p>
-      <p>Aqui va el contacto</p>
-      <p>Aqui va el footer</p>
+      <section className="container mt-5">
+        <h3 className="fst-italic">
+          Bienvenido a <span>Traveluxe</span> 
+          <i className="fa-solid fa-umbrella-beach ms-4"></i> 
+        </h3>
+        <h4 className="text-center mt-5">¡Conoce destinos que te sorprenderán!</h4>
+
+        <div id="carouselExample" className="carousel slide">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="" className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+
+        {/* agregar un link para enviar al contacto */}
+        {/* <p>Aqui va el contacto</p>
+        <p>Aqui va el footer</p> */}
+      </section>
     </>
   );
 };
